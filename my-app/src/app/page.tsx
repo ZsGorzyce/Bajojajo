4bdcfaa6d1083857c641156588b1c38e9b0e7482
Ivan <82017106+vanaf25@users.noreply.github.com>
2025-03-14 12:27:04 +0100

    tested version of chat

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -15,45 +15,9 @@ export const AcmeLogo = () => {
 };
 
 export default function Home() {
-
-
   return (
     <>
-      <HeroUIProvider>
-        <Navbar shouldHideOnScroll>
-          <NavbarBrand>
-            <AcmeLogo />
-            <p className="font-bold text-inherit">ACME</p>
-          </NavbarBrand>
-          <NavbarContent className="sm:flex gap-4" justify="center">
-            <NavbarItem>
-              <Link color="foreground" href="#">
-                Features
-              </Link>
-            </NavbarItem>
-            <NavbarItem>
-              <Link aria-current="page" href="#">
-                Customers
-              </Link>
-            </NavbarItem>
-            <NavbarItem>
-              <Link color="foreground" href="#">
-                Integrations
-              </Link>
-            </NavbarItem>
-          </NavbarContent>
-          <NavbarContent justify="end">
-            <NavbarItem className="lg:flex">
-              <Link href="#">Login</Link>
-            </NavbarItem>
-            <NavbarItem>
-              <Button as={Link} color="primary" href="#" variant="flat">
-                Sign Up
-              </Button>
-            </NavbarItem>
-          </NavbarContent>
-        </Navbar>
-      </HeroUIProvider>
+
     </>
   );
 }
